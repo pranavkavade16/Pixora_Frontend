08e64ff8d6a6f1633758898bde29f8470ea17360
pranavkavade16 <pranavkavade69739@gmail.com>
2026-06-13 13:00:11 +0530

    added mobile nav component

---
+++ components/MobileNav.jsx
@@ -0,0 +1,26 @@
+
+function MobileNav({ hidden }) {
+  return (
+    <nav
+      className={hidden ? "mobile-bottom-nav hidden" : "mobile-bottom-nav"}
+      aria-label="Mobile navigation"
+    >
+      <a className="active" href="/">
+        <ImageIcon size={28} />
+        Library
+      </a>
+      <a href="/">
+        <Star size={29} />
+        Starred
+      </a>
+      <a href="/">
+        <Search size={29} />
+        Explore
+      </a>
+      <a href="/">
+        <UserCircle size={29} />
+        Profile
+      </a>
+    </nav>
+  );
+}
